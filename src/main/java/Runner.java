c9d92e17fb8586a3cacd712497cc2906bd49b1cb
BerkhanSaral <137205976+BerkhanSaral@users.noreply.github.com>
2024-05-24 22:23:21 +0300

    v1 Berkhan

---
+++ src/main/java/Runner.java
@@ -0,0 +1,8 @@
+public class Runner {
+    public static void main(String[] args) {
+        //version 1
+        System.out.println("hello git");
+
+        System.out.println("git add. staging areya alir ");
+    }
+}
